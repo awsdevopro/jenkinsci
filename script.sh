5eacc53f09b57a1b85e36ccf0c60ec87831afbc6
Mamun <abdullah.mamun@itconquest.com>
2018-07-30 21:21:35 +0600

    ch

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Installing docker
-echo 'Installing latest Docker...' && \
-wget -qO- https://get.docker.com/ | sh && \
-sudo usermod -aG docker $USER  && \
-sudo systemctl enable docker
-usermod -aG docker jenkins
